fdc418234733be956e9b7e378b81d576a8025986
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-09-27 10:06:38 -0700

    hard work

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
+/**
+ * main - prints string
+ * Return: return the printed string and end the function
+ */
 int main(void)
 {
 char s[] = "\"Programming is like building a multilingual puzzle";
-
 puts(s);
-
 return (0);
-}
