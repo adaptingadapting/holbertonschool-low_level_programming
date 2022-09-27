85ae253f91b9db007771102464d298bb950e74c4
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-09-27 11:36:24 -0700

    added 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("Size of int: %zu bytes\n", sizeof(char));
-printf("Size of float: %zu bytes\n", sizeof(int));
-printf("Size of long int: %zu bytes\n", sizeof(long));
-printf("Size of long long: %zu byte\n", sizeof(long long));
-printf("Size of float: %zu byte\n", sizeof(float));
+  printf("Size of int: %zu byte(s)\n", sizeof(char));
+printf("Size of float: %zu byte(s)\n", sizeof(int));
+printf("Size of long int: %zu byte(s)\n", sizeof(long));
+printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
