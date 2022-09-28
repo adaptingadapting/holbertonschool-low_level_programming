494557a13d7fa0204c0936d731fbb4165166f8f5
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-09-27 19:10:30 -0700

    finished 8

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-fwrite("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 100, 1, stderr);
-exit(EXIT_FAILURE);
+fwrite("and that piece of art is useful\" \
+-Dora Korpar, 2015-10-19\n", 100, 1, stderr);
+return (1);
 }
