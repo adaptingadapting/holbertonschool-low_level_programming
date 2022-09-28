04b44f3f8f168b7df1e40db8ae71666d68f51b4b
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-09-28 11:37:24 -0700

    added 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints string
+ * Return: return the printed string and end the function
+ */
+int main(void)
+{
+int number;
+number = 97;
+while (number <= 122)
+{
+putchar(number);
+number++;
+}
+putchar(10);
+return (0);
+}
