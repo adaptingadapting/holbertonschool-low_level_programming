e698347b2aad6a48aa1edc147daa66c19ada7ce4
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-09-28 12:09:17 -0700

    work

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints string
+ * Return: return the printed string and end the function
+ */
+int main(void)
+{
+int number;
+number = 97;
+while (number <= 122)
+{
+   putchar(number);
+  number++;
+  }
+
+putchar(10);
+return (0);
+}
+if (number == 100 || 113)
+  {
+    number++;
+  } else {
+  
