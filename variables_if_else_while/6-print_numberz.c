dae2973ba19392f36f29e7c78410557a28473b4e
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-09-28 20:48:08 -0700

    added 5

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints string
+ * Return: return the printed string and end the function
+ */
+int main(void)
+{
+int number;
+number = 48;
+while (number <= 57)
+{
+putchar(number);
+number++;
+}
+putchar(10);
+return (0);
+}
