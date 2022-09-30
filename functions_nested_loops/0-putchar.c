053f622493d7f9e6b013af84cd4ffb2d35ab5b52
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-09-30 05:21:50 -0700

    added 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,18 +2,18 @@
 /**
  * main - prints word
  *
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
- _putchar('_');
- _putchar('p');
- _putchar('u');
- _putchar('t');
- _putchar('c');
- _putchar('h');
- _putchar('a');
- _putchar('r');
- _putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
