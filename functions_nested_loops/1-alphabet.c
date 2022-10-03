99a03e8e81c09ca9c25d075585a120f639130911
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-02 18:15:05 -0700

    added 1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 int main(void)
 {
 void print_alphabet(void)
@@ -7,13 +7,10 @@ void print_alphabet(void)
   int letter = 97;
         while (letter <= 122)
 	{
-	  putchar(letter);
+	  _putchar(letter);
 	  letter++;
 	}
-        putchar(10);
+       _putchar(10);
     
 }
- print_alphabet();
-    
-
 }
