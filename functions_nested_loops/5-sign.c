c3fcecb428f515812baf3adf5d2edc800d83e11e
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-02 18:56:02 -0700

    added 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,21 +1,27 @@
-int print_sign(int n);
+#include "main.h"
+/**
+ * print_sign - prints sign of an int
+ * @n: int to be evaluated
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+
+int print_sign(int n)
 {
-  int n;
-  if (n > 0)
-    {
-      printf("+");
-      return 1;
-    } else {
-    if (n < 0)
-      {
-	printf("-");
-	return -1;
-      } else {
-      printf("0");
-      return ("0");
-    }
-  }
+int i;
+if (n > 0)
+{
+ _putchar('+');
+i = 1;
 }
-      
-      
-  
+else if (n < 0)
+{
+_putchar('-');
+i = -1;
+}
+else
+{
+_putchar('0');
+i = 0;
+}
+return (i);
+}
