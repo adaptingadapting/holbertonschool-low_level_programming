58f7dae962baee9df0c3e25db097f673678a926d
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-02 18:33:12 -0700

    added 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,24 @@
 #include "main.h"
-#include <stdio.h>
-int main(void)
-{
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+* Return: void
+ */
+
 void print_alphabet(void)
 {
-  int letter = 97;
-  int count = 0;
-  while (count <= 10)
-    {
-      while (letter <= 122)
-	{
-	  putchar(letter);
-	  letter++;
-	}
-      letter = 97;  
-      count++;
-      putchar(10);
-    }
+int letter = 97;
+int count = 0;
+while (count <= 10)
+{
+while (letter <= 122)
+{
+_putchar(letter);
+letter++;
 }
- print_alphabet();
-    
+letter = 97;  
+count++;
+_putchar('\n');
+}
+}
 
-}
