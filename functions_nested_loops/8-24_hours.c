d3c5b7f5a09f330800d0a6449c56f13261b01948
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-02 19:13:20 -0700

    added 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,13 +1,38 @@
-#include <stdio.h>
-int main(void)
+
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: 0 if success
+ */
+
+void jack_bauer(void)
 {
-  int uno;
-  int dos;
-  int tres;
-  int cuatro;
-  for (uno = 0; uno < 3; uno++)
+  int a = 0;
+  int b = 0;
+  int c = 0;
+  int d = 0;
+
+  for (a = 0; a < 3; a++)
     {
-      printf("%d", uno);
-    
+      for (b = 0; b < 10 ; b++)
+	{
+	  for (c = 0; c < 6; c++)
+	    {
+	      for (d = 0; d < 10; d++)
+		{
+		  if (a == 2 && (b > 3))
+		    {
+		    } else {
+		    _putchar(a);
+		    _putchar(b);
+		    _putchar(':');
+		    _putchar(c);
+		    _putchar(d);
+		    _putchar('\n');
+		  }
+		}
+	    }
+	}
     }
 }
