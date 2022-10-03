83396c300309d5454623fc30b89a6bce17c485ce
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-02 18:49:33 -0700

    added 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,15 +1,16 @@
-int _isalpha(int c);
+#include "main.h"
+
+/**
+ * _isalpha - determines if input corresponds to alphabetic character
+ *
+ * @c: number to be evaluated
+ *
+ * Return: 1 if c is alphabetic character and 0 otherwise
+ */
+
+int _isalpha(int c)
 {
-  int c;
-    if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90)
-      {
-	return 1;
-      } else {
-      return (0);
-      }
-      
-      }
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
+return (0);
 }
-      
-      
-  
