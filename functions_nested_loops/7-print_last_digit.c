a1c8b4caaa179f1dceee861a3ec7a779ffa99590
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-02 19:03:56 -0700

    added 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,18 @@
-int print_last_digit(int a);
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of an int
+ * @i: integer of which we print the last digit
+ *
+ * Return: value of last digit
+ */
+
+int print_last_digit(int i)
 {
-  int a;
-  int b;
-  a % 10 = b;
-  return b;
+int result = i % 10;
+
+if (result < 0)
+result *= -1;
+
+_putchar(result + '0');
+return (result);
 }
-  
-    
