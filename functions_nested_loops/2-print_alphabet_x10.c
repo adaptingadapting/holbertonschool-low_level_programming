0b8b08cb08def57e0c579d7de7310efecccaf7f3
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-02 18:41:57 -0700

    added 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,21 +4,19 @@
  *
 * Return: void
  */
-
 void print_alphabet_x10(void)
 {
 int letter = 97;
 int count = 0;
-while (count <= 10)
+while (count < 10)
 {
 while (letter <= 122)
 {
 _putchar(letter);
 letter++;
 }
-letter = 97;  
+letter = 97;
 count++;
 _putchar('\n');
 }
 }
-
