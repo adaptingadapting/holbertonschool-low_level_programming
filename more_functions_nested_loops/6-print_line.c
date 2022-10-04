334c6884fbe3fa9613ab46a31e1b5fc7c5003d89
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-04 08:30:35 -0700

    added 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - print the 9 times table starting on 0
+ *
+ * Return: 0 on success
+ */
+
+void print_line(int n)
+{
+int x = 0;
+for (x = 0; x < n; x++)
+if (n <= 0)
+{
+putchar(10);
+return;
+}
+else
+{
+printf("_");
+}
+ putchar(10);
+}   
+
