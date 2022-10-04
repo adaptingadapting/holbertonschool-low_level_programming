8ee46294dfe2ff6bc49779f7572d242a0467c076
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-04 07:18:42 -0700

    added 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints alphabet 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+int letter = 0;
+int count = 0;
+while (count < 10)
+{
+while (letter <= 14)
+{
+printf("%d", letter);
+letter++;
+}
+letter = 0;
+count++;
+_putchar('\n');
+}
+}
