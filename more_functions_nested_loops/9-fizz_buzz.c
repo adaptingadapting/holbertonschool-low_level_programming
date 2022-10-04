3625924964dc6dec64b0b319e920489a5939a984
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-04 10:36:19 -0700

    added 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 
-void main(void)
+int main(void)
 {
 int number = 1;
- 
 for (number = 1; number <= 100; number++)
 {
-  if (number % 3 == 0 && number % 5 == 0)
-    {
-      printf("FizzBuzz ");
-	continue;
-	}
-  else if (number % 3 == 0)
-    {
-      printf("Fizz ");
-      continue;
-	}
-  else if (number % 5 == 0)
-    {
-      printf("Buzz ");
-      continue;
-    }
-       printf("%d ", number);
- }
+if (number % 3 == 0 && number % 5 == 0)
+{
+printf("FizzBuzz ");
+continue;
+}
+else if (number % 3 == 0)
+{
+printf("Fizz ");
+continue;
+}
+else if (number % 5 == 0)
+{
+printf("Buzz");
+if (number < 100)
+{
+printf(" ");
+}
+continue;
+}
+printf("%d ", number);
+}
+printf("\n");
+return (0);
 }
 
