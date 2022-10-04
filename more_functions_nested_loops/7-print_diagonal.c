54fe31cd455e98b983e75e30601ca64b41c09d0f
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-04 09:17:56 -0700

    added 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - print the 9 times table starting on 0
+ *@n - number
+ * Return: 0 on success
+ */
+
+void print_diagonal(int n)
+{
+int x = 0;
+int y = 0;
+
+if (n <= 0)
+{
+putchar(10);
+return;
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+  for (y = 0; y < x; y++)
+    {
+      printf(" ");
+    }
+printf("\\");
+printf("\n");
+}     
+putchar(10);
+}
+}
+
+
+
