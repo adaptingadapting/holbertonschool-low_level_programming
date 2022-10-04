eb9e6c1b3629b09464627271a5feb1e5402e2530
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-04 06:58:22 -0700

    added 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - determines if char is lowercase
+ *@a: int representing the ascii character to be evalated
+ *@b: int that represents second number
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
