bc78f19111d7ee87cb76681c24a35c30ca247ee7
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-04 06:17:00 -0700

    added last

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,27 @@
 #include <stdio.h>
 /**
  * print_to_98 - prints natural numbers between n & 98
-  * @n: number from which the count starts
-   * Return:void
-    */
+ * @n: number from which the count starts
+ * Return:void
+ */
 
 void print_to_98(int n)
 {
-for (98 - n; n < 98; n++)
-printf("%d", n);
+if (n < 98)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+}
+}
+else if (n > 98)
+{
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+}
+printf("%d\n", n);
 }
