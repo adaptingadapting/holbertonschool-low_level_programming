d86078f76d416d3bc231767c45e57de656946add
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-05 05:38:03 -0700

    added 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+int number = 48;
+while (number <= 57)
+{
+if (number == 59 || number == 61)
+{
+number++;
+continue;
+}
+else
+{
+_putchar(number);
+number++;
+}
+}
+_putchar('\n');
+}
