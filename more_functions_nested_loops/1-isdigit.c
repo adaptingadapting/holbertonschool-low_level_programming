48a3b89c5463263dd8ace8d9f6aa216036f04ece
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-05 05:15:27 -0700

    added 1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * _isalpha - determines if input corresponds to alphabetic character
+ * _isdigit - determines if input corresponds to alphabetic character
  *
  * @c: number to be evaluated
  *
  * Return: 1 if c is alphabetic character and 0 otherwise
  */
 
-int _isupper(int c)
+int main(void)
+{
+int _isdigit(int c)
 {
 if ((c >= 48 && c <= 57))
 return (1);
 return (0);
+}
 }
