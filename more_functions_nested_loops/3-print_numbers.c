fda8193479c755f185d349abf9ac8c6ee44bbab3
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-05 05:32:40 -0700

    added 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_numbers - prints alphabet in lowercase
  *
  * Return: void
  */
-void print_numbers(void);
+void print_numbers(void)
 {
 int number = 48;
 while (number <= 57)
 {
-_putchar(number);
-number++;
-}
+printf("%d", number);
+number++;}
+
 _putchar('\n');
 }
+
