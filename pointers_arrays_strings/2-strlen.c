71d5cb23184cb4b773884cd79711caba9ab81809
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-10 11:48:58 -0700

    small fix

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- *_strlen - function that returns an int that matches the lenght of the string 
+ *_strlen - function that returns an int that matches the lenght of the string
  * @s: the string in question
+ *Return: returns the lenght of the string (measured in i)
  */
 
 int _strlen(char *s)
