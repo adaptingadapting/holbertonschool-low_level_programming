56706ba57feaf60253473bb967f73b50a83aa556
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-11 05:32:46 -0700

    added 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *puts2 - prints every other character in a string
+ *@str: string thats going to be cut
+ */
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; i < _strlen(str); i++)
+{
+if (i % 2 == 0)
+{
+putchar(i + '0');
+continue;
+}
+}
+putchar(10);
+}
+/**
+ *_strlen - function that returns an int that matches the lenght of the string
+ * @s: the string in question
+ *Return: returns the lenght of the string (measured in i)
+ */
+
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*(s + i))
+i++;
+return (i);
+}
