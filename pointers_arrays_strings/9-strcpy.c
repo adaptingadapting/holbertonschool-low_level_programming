b4b4797eefc6f27b1409c7c7f6442dd0f525da43
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-11 07:59:29 -0700

    added 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcpy - function that copies a string into a buffer
+ *@dest: buffer to copy into
+ *@src: source to copy from
+ *Return: returns the string dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; i < _strlen(src); i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
+/**
+ *_strlen - function that returns an int that matches the lenght of the string
+ * @s: the string in question
+ *Return: returns the lenght of the string (measured in i)
+ */
+
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*(s + i))
+i++;
+return (i);
+}
