c6857a7da16c5ff2a928034885c113d47c767a41
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-11 08:16:13 -0700

    added 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -13,12 +13,9 @@ char *_strcpy(char *dest, char *src)
 int i;
 for (i = 0; i < _strlen(src); i++)
 {
-if (src[i] == '\0')
-{
-break;
-}
 dest[i] = src[i];
 }
+dest[i] = '\0';
 return (dest);
 }
 /**
