93d604a1320d32958ebf16db010c9f06128fdb9e
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-12 10:47:51 -0700

    fixed 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *string_toupper - the function that changes to upper  
+ *string_toupper - the function that changes to upper
  *@jero: the string to be changed to upper case
  *Return: returns the uppercase string
  */
@@ -12,10 +12,10 @@ char *string_toupper(char *jero)
 int i;
 for (i = 0; jero[i]; i++)
 {
-    if (jero[i] >= 97 && i <= 122 )
-      jero[i] = jero[i] - 32;
-    else
-      jero[i] = jero[i];
+if (jero[i] >= 97 && i <= 122)
+jero[i] = jero[i] - 32;
+else
+jero[i] = jero[i];
 }
 return (jero);
 }
