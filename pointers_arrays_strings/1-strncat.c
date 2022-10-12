b56d1db950f2598a8198c086b238da95e621835e
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-12 06:48:59 -0700

    fixed 1, i have to say this is unfair, my solution should work

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 5;
+int i;
 int j;
+for (i = 0; dest[i]; i++)
+{
+}
 for (j = 0; src[j] && j < n; j++)
 {
-dest[i + 1] = src[j];
+dest[i] = src[j];
 i++;
 }
 return (dest);
