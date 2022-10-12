4c9d90c86eae770863360ae05563f2fc23391776
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-12 08:26:41 -0700

    added 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,9 @@
 /**
  *_strcmp - compares two strings
  *@s1: string 1 (compares to the second one)
- *@s2: string 2 
- *Return: returns either a positive or a negative or 0 depending on the result of the comparison
+ *@s2: string 2
+ *Return: returns either a positive or a ne
+ *gative or 0 depending on the result of the comparison
  */
 
 int _strcmp(char *s1, char *s2)
@@ -10,15 +11,15 @@ int _strcmp(char *s1, char *s2)
 int res;
 if (s1 == s2)
 {
-  res = 0;
+res = 0;
 }
 else if (s1 > s2)
 {
-  res = 15;
+res = (-13);
 }
 else
 {
-  res = (-15);
+res = (13);
 }
 return (res);
 }
