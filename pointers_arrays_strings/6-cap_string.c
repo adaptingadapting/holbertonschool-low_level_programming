05416e7ced960190c866b37ffc5e448361aaf5f5
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-12 11:07:21 -0700

    added 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *cap_string - capitalizes the input string, all words
+ *@s: string to be capitalized
+ *Return: returns the capitalized string
+ */
+
+char *cap_string(char *s)
+{
+int i;
+for (i = 0; s[i]; i++)
+if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
+s[i] == ',' || s[i] == '.' || s[i] == '.' ||
+s[i] == ';' || s[i] == '(' || s[i] == ')' ||
+s[i] == '}' || s[i] == '{' || s[i] == '!' || s[i] == '?')
+{
+if (s[i + 1] >= 95 &&  s[i + 1] <= 122)
+s[i + 1] = s[i + 1] - 32;
+}
+return (s);
+}
