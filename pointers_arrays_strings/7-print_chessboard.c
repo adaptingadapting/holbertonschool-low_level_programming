d0e478f9331c1610e5f83486d38aeee4485a9435
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-10-13 12:42:57 -0700

    added 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints something
+ * @a: a
+ * Return: returns
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c", (a[i][j]));
+if (j == 7)
+printf("\n");
+}
+}
+}
