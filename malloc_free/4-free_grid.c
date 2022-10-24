c65699a5e4b4d889ababcc05e17dde8746e7193e
root <elpatoninjasuperninja@gmail.com>
2022-10-24 02:10:25 -0700

    fixed 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *free_grid- free grid
+ *@grid: grid
+ *@height: height
+ *Return: 0
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
