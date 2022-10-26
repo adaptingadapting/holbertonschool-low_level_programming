f0ac9c5e1265103f51ecc54b3bae8e30b457331a
root <elpatoninjasuperninja@gmail.com>
2022-10-26 12:42:28 -0700

    added 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _calloc - allocates memory for arra
+ * @nmemb: elements in array
+ * @size: size
+ * Return: something
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+int k;
+void *p;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+k = nmemb * size;
+p = malloc(k);
+if (p == NULL)
+return (NULL);
+
+memset(p, 0, k);
+return (p);
+}
