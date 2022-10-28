b1aee3337d96ff6d9182440bbd09b54781db589b
root <elpatoninjasuperninja@gmail.com>
2022-10-28 08:28:46 -0700

    added 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ *init_dog - description
+ *@d: d
+ *@name: name
+ *@age: age
+ *@owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog;
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+return;
+}
+}
