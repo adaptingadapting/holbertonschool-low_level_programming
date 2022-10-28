03755ad04d3d6064675297eb54a8288f6fb68422
root <elpatoninjasuperninja@gmail.com>
2022-10-28 07:05:31 -0700

    added dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef hola_h
+#define hola_h
+/**
+ *struct dog - dog
+ *@name: name
+ *@age: age
+ *@owner: owner
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog;
+#endif
