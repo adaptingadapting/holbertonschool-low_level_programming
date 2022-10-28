339648e6130ff0ad765875eab2f4e313a2e89622
root <elpatoninjasuperninja@gmail.com>
2022-10-28 08:53:26 -0700

    added 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,20 +1,20 @@
 #include "dog.h"
-#include <stdio.h>
 /**
- *init_dog - description
+ *init_dog - function
  *@d: d
  *@name: name
  *@age: age
  *@owner: owner
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-struct dog;
+if (!d)
 {
+return;
+}
+
 d->name = name;
 d->age = age;
 d->owner = owner;
 return;
-}
 }
