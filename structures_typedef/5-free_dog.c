2b322f457c00012413546281d5cec170cc63f3d5
root <elpatoninjasuperninja@gmail.com>
2022-11-01 18:10:10 -0700

    added 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * free_dog - free the memory used by a dog
+ * @d: pointer to struct dog
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+if (!(d))
+return;
+free(d->name);
+free(d->owner);
+free(d);
+}
