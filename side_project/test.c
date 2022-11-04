2a2b7d740ed7abc9e35d3ac2a4164ae638451fe2
root <elpatoninjasuperninja@gmail.com>
2022-11-03 22:22:42 -0700

    added changgeS

--- side_project/test.c
+++ side_project/test.c
@@ -6,18 +6,19 @@
 
 void print(long long int n)
 {
-  if (n < 0) {
+  if (n < 0)
+    {
     putchar('-');
     n = -n;
-  }
-  if (n/10)
-    print(n/10);
-  putchar(n%10 + '0');
+    }
+  if (n / 10)
+    print(n / 10);
+  putchar(n % 10 + '0');
 }
 void _prontf(const char *format, va_list args)
 {
   int ctr = 0;
-  int foundPercent = 0; 
+  int foundPercent = 0;
   while (format[ctr])
     {
       if (!(foundPercent))
@@ -34,7 +35,7 @@ void _prontf(const char *format, va_list args)
       else if (foundPercent)
 	{
 	  switch (format[ctr])
-	    {	    
+	    {
 	    case 's':
 	      {
 		char *StringFormatReplacement = va_arg(args, char *);
@@ -54,7 +55,7 @@ void _prontf(const char *format, va_list args)
 	    case 'd':
 	      {
 		int DecimalFormatReplacement = va_arg(args, int);
-		print(DecimalFormatReplacement); 
+		print(DecimalFormatReplacement);
 		break;
 	      }
 	    case 'i':
@@ -98,30 +99,44 @@ int _printf(const char *format, ...)
 	  if (format[strlen + 1] == escapeSeqArray[arrayIterator])
 	    {
 	      strlen++;
-	      escapeSequences =+ 1;
+	      escapeSequences += 1;
 	    }
       }
   return (strlen - escapeSequences);
 }
-int main()
+int main(void)
 {
   int len;
   int len2;
   unsigned int ui;
   void *addr;
+  ui = (unsigned int)INT_MAX + 1024;
+  addr = (void *)0x7ffe637541f0;
   len = _printf("Let's try to printf a simple sentence.\n");
+  len2 = printf("Let's try to printf a simple sentence.\n");
   ui = (unsigned int)INT_MAX + 1024;
   addr = (void *)0x7ffe637541f0;
   _printf("Length:[%d, %i]\n", len, len);
+  printf("Length:[%d, %i]\n", len2, len2);
   _printf("Negative:[%d]\n", -762534);
+  printf("Negative:[%d]\n", -762534);
   _printf("Unsigned:[%u]\n", ui);
+  printf("Unsigned:[%u]\n", ui);
   _printf("Unsigned octal:[%o]\n", ui);
+  printf("Unsigned octal:[%o]\n", ui);
   _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+  printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
   _printf("Character:[%c]\n", 'H');
+  printf("Character:[%c]\n", 'H');
   _printf("String:[%s]\n", "I am a string !");
+  printf("String:[%s]\n", "I am a string !");
   _printf("Address:[%p]\n", addr);
+  printf("Address:[%p]\n", addr);
   len = _printf("Percent:[%%]\n");
+  len2 = printf("Percent:[%%]\n");
   _printf("Len:[%d]\n", len);
+  printf("Len:[%d]\n", len2);
   _printf("Unknown:[%r]\n");
+  printf("Unknown:[%r]\n");
   return (0);
 }
