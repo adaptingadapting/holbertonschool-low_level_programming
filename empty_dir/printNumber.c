08fc1823a600e3efac3a0d2a5bccc7cb6aa08484
root <elpatoninjasuperninja@gmail.com>
2022-11-04 12:07:33 -0700

    print

--- empty_dir/printNumber.c
+++ empty_dir/printNumber.c
@@ -1,28 +1,38 @@
 #include "main.h"
 
-void FromDeci(long n, int base) 
+int main(void)
 {
-  long decimalnum, quotient, remainder;
-  int i, j = 0;
-  char hexadecimalnum[100];
-  decimalnum = n;
-  quotient = decimalnum;
-  while (quotient != 0)
-    {
-      remainder = quotient % base;
-      if (remainder < 10)
-	hexadecimalnum[j++] = 48 + remainder;
-      else
-	hexadecimalnum[j++] = 55 + remainder;
-      quotient = quotient / base;
-    }
+  int len;
+  int len2;
+  unsigned int ui;
+  void *addr;
   
-  for (i = j; i >= 0; i--)
-    putchar(hexadecimalnum[i]);
+  len = _printf("Let's try to printf a simple sentence.\n");
+  len2 = printf("Let's try to printf a simple sentence.\n");
+  ui = (unsigned int)INT_MAX + 1024;
+  addr = (void *)0x7ffe637541f0;
+  _printf("Length:[%d, %i]\n", len, len);
+  printf("Length:[%d, %i]\n", len2, len2);
+  _printf("Negative:[%d]\n", -762534);
+  printf("Negative:[%d]\n", -762534);
+  _printf("Unsigned:[%u]\n", ui);
+  printf("Unsigned:[%u]\n", ui);
+  _printf("Unsigned octal:[%o]\n", ui);
+  printf("Unsigned octal:[%o]\n", ui);
+  _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+  printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+  _printf("Character:[%c]\n", 'H');
+  printf("Character:[%c]\n", 'H');
+  _printf("String:[%s]\n", "I am a string !");
+  printf("String:[%s]\n", "I am a string !");
+  _printf("Address:[%p]\n", addr);
+  printf("Address:[%p]\n", addr);
+  len = _printf("Percent:[%%]\n");
+  len2 = printf("Percent:[%%]\n");
+  _printf("Len:[%d]\n", len);
+  printf("Len:[%d]\n", len2);
+  _printf("Unknown:[%r]\n");
+  printf("Unknown:[%r]\n");
+  return (0);
 }
-void main(void)
-{
-  long int a = 12200;
-  int base = 16;
-  FromDeci(a, base);
-}
+
