1f8766a9754581b0d726d5e5aed7ea3627c5354c
root <elpatoninjasuperninja@gmail.com>
2022-11-16 04:07:51 -0800

    2

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 
 /**
- * _free_list - Realease the memory allocated for a list
- *
+ * free_list - Realease the memory allocated for a list
  * @head: A pointer to the first node of the list to free
  */
 void free_list(list_t *head)
