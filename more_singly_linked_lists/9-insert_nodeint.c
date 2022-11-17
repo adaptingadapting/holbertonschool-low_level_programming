92b4c78afb23d48ae2dc10a960f88fb2ed370a7f
root <elpatoninjasuperninja@gmail.com>
2022-11-17 09:28:43 -0800

    ad

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - hola
+ * @h: where
+ * @idx: where
+ * @number: what
+ * Return: 0
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **h, unsigned int idx, int number)
+{
+	unsigned int i = 0;
+	listint_t *node = *h;
+	listint_t *newnode = malloc(sizeof(listint_t));
+	listint_t *tmp;
+
+	if (!node)
+		return (NULL);
+
+	while (node)
+	{
+		if (i == idx - 1)
+		{
+			tmp = node->next;
+			node->next = newnode;
+			newnode->n = number;
+			newnode->next = tmp;
+		}
+		node = node->next;
+		i++;
+	}
+	return (node);
+}
