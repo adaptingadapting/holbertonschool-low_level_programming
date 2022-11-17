1e3b13ea8bc02972c3fbecb9857918106f5f28b3
root <elpatoninjasuperninja@gmail.com>
2022-11-17 07:58:41 -0800

    ade

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -20,6 +20,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+listint_t *get_nodeint_at_index(listint_t *h, unsigned int index);
 int pop_listint(listint_t **head);
 void free_listint(listint_t *head);
 size_t print_listint(const listint_t *h);
