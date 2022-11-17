26369083fe5612606ce0ba3236e23f8f54b319c3
root <elpatoninjasuperninja@gmail.com>
2022-11-17 05:11:40 -0800

    added 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,6 +1,19 @@
 #include "lists.h"
 
 /**
+ * free_listint - d
+ * @head: head
+ */
+
+void free_listint(listint_t *head)
+{
+	if (head)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
+/**
  * free_listint2 - func
  * @head: head
  */
