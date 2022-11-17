bad32d6aa156dc19f9369d6917c8de133cf71114
root <elpatoninjasuperninja@gmail.com>
2022-11-17 07:48:36 -0800

    added

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - prints all the elements of a list
+ * @h: pointrr to the first element
+ * @index: ola
+ * Return: number of nodes
+ */
+
+listint_t *get_nodeint_at_index(listint_t *h, unsigned int index)
+{
+	int i = 0;
+
+	if (!h)
+		return (0);
+
+	while (h && i < index)
+		{
+		if (!(h->n))
+			;/*	printf("%d", 0);*/
+		if (h->n)
+			;/*	printf("%d\n", h->n);*/
+		h = h->next;
+		i++;
+		}
+	return (h);
+}
+
+
