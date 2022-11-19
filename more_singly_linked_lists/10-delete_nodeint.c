b25a7aeffafa4c567cafb43cd6d0e21da4890f4c
root <elpatoninjasuperninja@gmail.com>
2022-11-19 07:12:12 -0800

    a

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int idx)
 {
 	listint_t *j = NULL, *tmp = NULL;
 	unsigned int i = 0;
-	
+
 	if (*head == NULL)
 		return (-1);
 	if (idx == 0)
