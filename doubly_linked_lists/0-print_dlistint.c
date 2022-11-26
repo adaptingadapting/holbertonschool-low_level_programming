f7282f1fd7a57059bb0bb7d89e5d6da47ad7c0d5
root <elpatoninjasuperninja@gmail.com>
2022-11-25 20:18:19 -0800

    adedde slisr

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the elements of a dlistint_tlist
+ * @h: name of the lists
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		return (0);
+	for (i = 0; h != NULL; i++)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
+	return (i);
+}
