f2cfc9936a2e20793e756973b5669105e2f8cfbd
root <elpatoninjasuperninja@gmail.com>
2022-11-27 14:43:22 -0800

    easy

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - hola
+ * @size: hola
+ * Return: hola
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i;
+	hash_table_t *table = NULL;
+
+	table = malloc(sizeof(hash_table_t));
+	if (!table)
+		return (NULL);
+	table->array = malloc(sizeof(hash_table_t));
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
