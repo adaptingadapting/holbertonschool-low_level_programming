1da61e66c5adc661993d7be93da34033d5fe1139
root <elpatoninjasuperninja@gmail.com>
2022-11-27 08:51:28 -0800

    betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -13,9 +13,9 @@ size_t print_dlistint(const dlistint_t *h)
 	if (h == NULL)
 		return (0);
 	for (i = 0; h != NULL; i++)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-		}
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 	return (i);
 }
