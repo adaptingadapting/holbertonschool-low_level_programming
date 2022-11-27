ee158216a5b58ec00998d8813b7aa2fda9b85b98
root <elpatoninjasuperninja@gmail.com>
2022-11-27 14:56:26 -0800

    added

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (!table->array)
 	{
 		free(table);
