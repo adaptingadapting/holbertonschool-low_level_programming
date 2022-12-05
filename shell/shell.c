99b281340ad7463f07b583219c7b535e04035cd2
root <elpatoninjasuperninja@gmail.com>
2022-12-05 08:45:53 -0800

    now works for ls and /usr/bin/ls with flags

--- shell/shell.c
+++ shell/shell.c
@@ -3,53 +3,56 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include "main.h"
 
-int main()
+/**
+ * main - Simple shell program
+ * Return: Characters
+ */
+
+int main(void)
 {
 	int i = 0;
 	char delim[] = {" \n:\t"};
 	char *token[80];
-	char *user_input = NULL;
+	char *userInput = NULL;
 	size_t buffsize;
+	char *statReturn;
 	size_t characters = 0;
-	int size;
 	pid_t parent = getpid();
 	pid_t pid;
 	int status;
-	
-	user_input = (char *)malloc(buffsize * sizeof(char));
-	if (!user_input)
+
+	userInput = (char *)malloc(buffsize * sizeof(char));
+	if (!userInput)
 		exit(1);
+
 loop:
 	for (;;)
 	{
 		printf("$ ");
-		characters = getline(&user_input, &buffsize, stdin);
-		token[0] = strtok(user_input, delim);
+		characters = getline(&userInput, &buffsize, stdin);
+		token[0] = strtok(userInput, delim);
 		if (!token[0])
 			goto loop;
 		if (!strcmp(token[0], "exit"))
 		{
-			free(user_input);
+			free(userInput);
 			exit(0);
 		}
 		i = 0;
 		while (token[i])
-		  {
+		{
 			i++;
 			token[i] = strtok(NULL, delim);
-		  }
-		pid = fork();
-		if (pid == -1)
-			perror("failed to fork");
-		else if (pid > 0)
-			wait(&status);
-		else 
-		{
-			if (execve(token[0],token,NULL) == -1)
-				printf("$ %s is not a recognized command\n", token[0]);
-			exit(1);
 		}
-	}
+		if (programStat(token[0]))
+		{
+			statReturn = programStat(token[0]);
+			printf("%s executed\n", statReturn);
+			token[0] = statReturn;
+		}
+		executePathProgram(token);
+		}
 	return (0);
 }
