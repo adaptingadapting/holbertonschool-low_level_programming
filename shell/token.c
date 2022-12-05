1b499e72c4ff667e0ccea80baabc3622e9a80233
root <elpatoninjasuperninja@gmail.com>
2022-12-05 12:36:10 -0800

    changs

--- shell/token.c
+++ shell/token.c
@@ -10,11 +10,34 @@ char *programStat(char *line)
 {
 	char *token;
 	char *i = getenv("PATH");
-	char *cp = malloc(strlen(i));
-	char *cp2 = malloc(strlen(line));
-	char *duptoken = malloc(strlen(line) + strlen(i) + 2);
+	char *cp = malloc(200);
+	char *cp2 = malloc(200);
+	char *duptoken = malloc(300);
 	struct stat st;
-
+	if (!cp)
+	{
+		if (cp2)
+			free(cp2);
+		if (duptoken)
+			free(duptoken);
+		return NULL;
+	}
+	if (!cp2)
+	{
+		if (cp)
+			free(cp);
+		if (duptoken)
+			free(duptoken);
+		return NULL;
+	}
+	if (!duptoken)
+	{
+		if (cp)
+			free(cp);
+		if (cp2)
+			free(cp2);
+		return NULL;
+	}
 	strcpy(cp, i);
 	strcpy(cp2, line);
 	token = strtok(cp, ":");
@@ -24,8 +47,16 @@ char *programStat(char *line)
 		strcat(duptoken, "/");
 		strcat(duptoken, cp2);
 		if (stat(duptoken, &st) == 0)
+		{
+			free(cp);
+			free(cp2);
+			duptoken;
 			return (duptoken);
+		}
 		token = strtok(NULL, ":");
 	}
+	free(cp);
+	free(cp2);
+	free(duptoken);
 	return (NULL);
 }
