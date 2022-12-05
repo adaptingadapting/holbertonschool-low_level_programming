49e5f58e7339977ac2324e0874f515fec6219d05
root <elpatoninjasuperninja@gmail.com>
2022-12-05 08:46:29 -0800

    added functions to main

---
+++ shell/main.h
@@ -0,0 +1,16 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+char *programStat(char *line);
+void executePathProgram(char **token);
+void executeProgram(char *str, char **token);
+
+#endif
