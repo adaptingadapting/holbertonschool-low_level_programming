62418f5783d54afb7601decbf6891292d5de7f48
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-22 19:52:17 -0300

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,64 +1,57 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_separator - print separator
- * @format: is a list of types of arguments passed to the function
- * @i: integer
- * Return: void
+ * print_all - formatted printing
+ * @format: format to print
  */
 
-void print_separator(const char * const format, int i)
+void print_all(const char *const format, ...)
 {
-if (format != NULL && format[i + 1] != '\0')
-printf(", ");
+	int i = 0;
+	va_list ap;
+	char *aux;
+	int separator = 0;;
+
+	va_start(ap, format);
+	while (format[i])
+	{
+		print_sep(separator, format[i + 1]);
+		separator = 0;		
+		switch (format[i])
+		{
+		case 'i':
+			separator = printf("%d", va_arg(ap, int));
+			break;
+		case 'c':
+			separator = printf("%c", va_arg(ap, int));
+			break;
+		case 's':
+			aux = va_arg(ap, char*);
+			if (aux)
+			{
+				separator = printf("%s", aux);
+				break;
+			}
+			separator = printf("(nil)");
+			break;
+		case 'f':
+			separator = printf("%f", va_arg(ap, double));
+			break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
 }
 
 /**
- * print_all - prints anything
- * @format: list
+ * print_sep - prints a speratror if needed
+ * @string: format
+ * @i: position of the format
  */
 
-void print_all(const char * const format, ...)
+void print_sep(int i, char c)
 {
-int i = 0;
-char *aux;
-va_list ap;
-
-va_start(ap, format);
-while ((format) && (format[i]))
-{
-switch (format[i])
-{
-case 'c':
-printf("%c", va_arg(ap, int));
-print_separator(format, i);
-break;
-case 'i':
-printf("%d", va_arg(ap, int));
-print_separator(format, i);
-break;
-case 'f':
-printf("%f", va_arg(ap, double));
-print_separator(format, i);
-break;
-case 's':
-aux = va_arg(ap, char*);
-if (aux)
-{
-printf("%s", aux);
-print_separator(format, i);
-}
-else
-{
-printf("(nil)");
-print_separator(format, i);
-}
-break;
-}
-i++;
-}
-printf("\n");
-va_end(ap);
+	if (i && c != '\0')
+		printf(", ");
 }
