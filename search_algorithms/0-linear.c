c165724bb3886da6066e7f0add29aead8f8e5476
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-07 10:03:07 -0300

    task 0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches in an array using the
+ * linear search algorithm
+ * @array: array of ints
+ * @size: size of the array
+ * @value: value to search for
+ * Return: -1 on failure
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (value == array[i])
+			return (i);
+	}
+	return (-1);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
