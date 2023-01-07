8dab8335b48dedc96555def778863bda0a4eb831
adaptingadapting <elpatoninjasuperninja@gmail.com>
2023-01-07 11:25:10 -0300

    hotfix

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -14,45 +14,29 @@ int binary_search(int *array, size_t size, int value)
 	size_t floor = 0;
 	size_t half;
 
+	if (!array)
+		return (-1);
 	size = size - 1;
-	printf("Searching in the array: ");
-	for (i = floor; i <= size; i++)
-	{
-		printf("%d", array[i]);
-		if (i < size)
-			printf(", ");
-	}
-	printf("\n");	
 	while (half != size)
 	{
+		printf("Searching in array: ");
+		for (i = floor; i <= size; i++)
+		{
+			printf("%d", array[i]);
+			if (i < size)
+				printf(", ");
+		}
+		printf("\n");	
 		half = ((floor + size) / 2);
 		if (array[half] == value)
 			return (half);
 		else if (array[half] < value)
 		{
 			floor = half + 1;
-			if (floor <= size)
-				printf("Searching in the array: ");
-			for (i = floor; i <= size; i++)
-			{
-				printf("%d", array[i]);
-				if (i < size)
-					printf(", ");
-			}
-			printf("\n");
 		}
 		else
 		{
 			size = half - 1;
-			if (floor <= size)
-				printf("Searching in the array: ");
-			for (i = floor; i <= size; i++)
-			{
-				printf("%d", array[i]);
-				if (i < size)
-					printf(", ");
-			}
-			printf("\n");
 		}
 	}
 	return (-1);
